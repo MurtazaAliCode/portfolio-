95d20266e4803115449425970ef0c74d8c00847c
MurtazaAliCode <syedmurtazaali756@gmail.com>
2025-11-18 16:32:52 +0500

    feat: display full resume preview in a modal

--- client/src/pages/resume.tsx
+++ client/src/pages/resume.tsx
@@ -40,14 +40,22 @@ export default function Resume() {
             </div>
           </div>
 
-          {/* Full Preview (Conditional) */}
+          {/* Full Preview Modal (Conditional) */}
           {showFullPreview && (
-            <div className="relative w-full md:w-2/3 lg:w-3/4 h-[800px] border border-gray-700 rounded-lg">
-              <iframe
-                src={resumePath}
-                title="Syed Murtaza Ali Full Resume"
-                className="w-full h-full border-0"
-              />
+            <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75">
+              <div className="relative w-11/12 h-5/6 bg-slate-800 rounded-lg p-4">
+                <button
+                  onClick={() => setShowFullPreview(false)}
+                  className="absolute top-2 right-2 text-white bg-red-600 hover:bg-red-700 rounded-full p-2"
+                >
+                  <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+                </button>
+                <iframe
+                  src={resumePath}
+                  title="Syed Murtaza Ali Full Resume"
+                  className="w-full h-full border-0"
+                />
+              </div>
             </div>
           )}
         </div>
